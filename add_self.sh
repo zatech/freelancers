94ff2c5f1088a66a9dc4d4d7fcb3add9e55fd08a
Christian Witts <cwitts@gmail.com>
2018-08-16 12:41:05 +0200

    Add self-service shell script
    The script asks the user for the relevant fields, and adds them
    to the table in the lexographically sorted position.
    
    Signed-off-by: Christian Witts <cwitts@gmail.com>

---
+++ add_self.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# Get new users details
+read -p 'Name & Surname: ' name
+read -p '/people/ filename: ' filename
+read -p 'Website: ' website
+read -p 'Skill set: ' skillset
+read -p 'Github Handle: ' github
+
+# Add new user to the README before sorting
+echo "[${name}](/people/${filename}) | ${website} | ${skillset} | [GitHub](https://github.com/${github})" >> README.md
+
+sed -e 's/# Jane Doe/# '"${name}"'/' people/janedoe.md > people/"${filename}".md
+
+# Get the line number for the top of the table, and skip them
+LNR=$(grep -n 'Name |' README.md | cut -f1 -d:)
+LNR=$(($LNR+2))
+
+# Sort the table lexographically by name
+SORTED_TABLE=$(tail -n +"${LNR}" README.md | sort --field-separator=\| --key=1)
+
+# Make a backup of the README before deleting the table and adding the sorted table
+sed -i.backup ${LNR}',$d' README.md
+printf "%s" "${SORTED_TABLE}" >> README.md
